c0373ab2f994fc7ad43775c9b6c52a6044551d11
DarkCompet <darkcompet@gmail.com>
2022-07-15 23:06:34 +0700

    Try/catch RunAsync

--- src/command.ts
+++ src/command.ts
@@ -2,23 +2,35 @@ import util from 'node:util';
 const execAsync = util.promisify(require('node:child_process').exec);
 
 import { DkBuildConfig } from './build-config';
+import { RunCommandResult } from './model';
 
 // For execute command line in system.
 export class DkCommand {
 	/**
-	 * Run a command in async. Caller should use `await` when call this.
+	 * Run a command in async. Caller should use `await` to wait result from this.
 	 *
 	 * @param {string} command for eg,. "cd /home/user/workspace; mkdir test; touch test/tmp.txt;"
 	 *
 	 * @returns An object that contains 2 outputs: stdout string for succeed, stderr string for error.
-	 * Caller can access to string when succeed via `result.stdout`.
-	 * And access to string when failed via `result.stderr`.
 	 */
-	static async RunAsync(command: string) {
-		if (DkBuildConfig.DEBUG) {
-			console.log(`Run command: ${command.trim()}`);
-		}
+	static async RunAsync(command: string): Promise<RunCommandResult> {
+		try {
+			if (DkBuildConfig.DEBUG) {
+				console.log(`Run command: ${command.trim()}`);
+			}
 
-		return await execAsync(command);
+			const result = await execAsync(command);
+
+			return {
+				stdout: result.stdout,
+				stderr: result.stderr
+			};
+		}
+		catch (e: any) {
+			return {
+				stdout: null,
+				stderr: e.message
+			};
+		}
 	}
 }
---
+++ src/model.ts
@@ -0,0 +1,5 @@
+
+export interface RunCommandResult {
+	stdout?: string|null;
+	stderr?: string|null;
+}
