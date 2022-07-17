8574ed43f9af4ae2fa411d4dcd40e4540d78c37d
DarkCompet <darkcompet@gmail.com>
2022-07-17 08:05:44 +0700

    Change double to single

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@
 /// Only files be exported here can be used at other modules.
 
 export * from './build-config';
+export * from './commander';
+export * from './file-system';
+export * from './model';
 export * from "./unix-shell";
-export * from "./model";
