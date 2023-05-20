3f691e8a2840a49e225f16b73385bde3a98b4b8a
DarkCompet <darkcompet@gmail.com>
2023-05-20 22:04:45 +0700

    Remove buggable funs

--- src/unix-shell.ts
+++ src/unix-shell.ts
@@ -5,30 +5,6 @@ import { RunCommandResult } from './model';
  * Handle with bash (Unix shell) command in Unix-based OS like Linux, Ubuntu,...
  */
 export class DkUnixShell {
-	static async FileExisted(filePath: string): Promise<boolean> {
-		const result = await DkCommander.RunAsync(`
-			if [[ -f "${filePath}" ]]; then
-				echo "true";
-			else
-				echo "false";
-			fi
-		`);
-
-		return result.stdout?.trim() == "true";
-	}
-
-	static async DirectoryExisted(dirPath: string): Promise<boolean> {
-		const result = await DkCommander.RunAsync(`
-			if [[ -d "${dirPath}" ]]; then
-				echo "true";
-			else
-				echo "false";
-			fi
-		`);
-
-		return result.stdout?.trim() == "true";
-	}
-
 	/**
 	 * By using `mkdir` command, this create directory recursively (option -p) if not exist.
 	 * This is similar to `DkFiles.MkDirs()`.
